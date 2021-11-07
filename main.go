c9e01e9ea2780f53918de80882ad837f953441fb
Heber Senger <sengerhe@gmail.com>
2021-11-07 10:41:04 -0300

    Initial import

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+var Router *gin.Engine
+
+func main() {
+	Router = gin.Default()
+	setupApi()
+
+	Router.Run(":" + getPort())
+}
+
+func getPort() string {
+	var port = os.Getenv("PORT")
+	if port == "" {
+		return "5000"
+	}
+	return port
+}
+
+func error(ctx *gin.Context, message string) {
+	ctx.JSON(500, gin.H{
+		"data": gin.H{
+			"message": message,
+		},
+	})
+}
+
+func setupApi() {
+	api := Router.Group("/api")
+	api.GET("/status", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"data": gin.H{
+				"message": "Server is online",
+			},
+		})
+	})
+
+	api.POST("/identify", identify)
+}
+
+type identifyRequest struct {
+	username string `json:username`
+}
+
+func identify(ctx *gin.Context) {
+	var request identifyRequest
+	if ctx.BindJSON(&request) != nil {
+		error(ctx, "Invalid request body")
+		return
+	}
+
+	ctx.JSON(200, gin.H{
+		"data": gin.H{
+			"deviceId": uuid.NewString(),
+			"username": request.username,
+		},
+	})
+}
